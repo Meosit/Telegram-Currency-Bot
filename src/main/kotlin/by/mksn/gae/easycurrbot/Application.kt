7d95a23d69e4bec1908482813a2167a2595a6232
Mikhail Snitavets <mksn13@gmail.com>
2019-05-16 18:11:15 +0300

    Fix first request ignoring issue

--- src/main/kotlin/by/mksn/gae/easycurrbot/Application.kt
+++ src/main/kotlin/by/mksn/gae/easycurrbot/Application.kt
@@ -10,19 +10,17 @@ import com.google.gson.FieldNamingPolicy
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
-import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.gson.gson
 import io.ktor.routing.routing
 import io.ktor.util.date.GMTDate
+import io.ktor.util.date.minus
 
 fun Application.main() {
     install(DefaultHeaders)
-    install(CallLogging)
     install(ContentNegotiation) {
         gson {
             setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
@@ -36,7 +34,7 @@ fun Application.main() {
         }
     }
     val config: AppConfig = AppConfig.create("application.conf")
-    val serverStartTime = GMTDate()
+    val serverStartTime = GMTDate() - 60000 // to handle first request after long startup
 
     val exchange = ExchangeRateService(httpClient, config)
     val service = CombinedService(
